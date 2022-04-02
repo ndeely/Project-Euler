3d25e1b11415fc641faa8832620c233a39dc824e
Niall Deely <nialldeely@gmail.com>
2022-04-02 21:16:24 +0100

    Added number selector in main to choose problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        new Problem17();
+        String val = "";
+
+        while (!val.equals("e")) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter Problem Number or type 'e' to exit:");
+
+            val = input.nextLine();
+            if (val.equals("e")) {
+                System.exit(0);
+            }
+
+            try {
+                invokeMethod("Problem" + val);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    private static void invokeMethod(String methodName) throws Exception {
+        Class c = Class.forName(methodName);
+        c.newInstance();
     }
 }
